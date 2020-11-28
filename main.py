23a56a18faca3ee56102f9b69e54709d173e2ad3
maany <imptodefeat@gmail.com>
2020-11-28 19:54:44 +0100

    test uploading

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ class SQCLI:
 
 if __name__ == '__main__':
     client_michal = SQlient("michal", "3.121.232.99:9000", "minioadmin", "minioadmin")
+    client_michal.qrypt.encrypt_file("data/file1.txt")
+    client_michal.mount10.put_object(client_michal.buckets[0], "file1", "data/file1.txt")
