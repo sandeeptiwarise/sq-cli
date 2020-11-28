fbdc2234127cc1dcc9749b580f8d531f9d182a4e
maany <imptodefeat@gmail.com>
2020-11-28 16:19:52 +0100

    sample upload

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from minio.error import ResponseError
 
 
 # Initialize minioClient with an endpoint and access/secret keys.
-minioClient = Minio('play.min.io',
+minioClient = Minio('3.121.232.99:9000',
                   access_key='minioadmin',
                   secret_key='minioadmin',
-                  secure=True)
+                  secure=False)
 from minio import Minio
 from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
                          BucketAlreadyExists)
@@ -15,7 +15,7 @@ from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
 
 # Make a bucket with the make_bucket API call.
 try:
-       minioClient.make_bucket("sq_test_bucket", location="us-east-1")
+       minioClient.make_bucket("sq-test-bucket", location="us-east-1")
 except BucketAlreadyOwnedByYou as err:
        pass
 except BucketAlreadyExists as err:
@@ -25,6 +25,6 @@ except ResponseError as err:
 
 # Put an object 'pumaserver_debug.log' with contents from 'pumaserver_debug.log'.
 try:
-       minioClient.fput_object('sq_test_bucket', 'file1.txt', 'data/file1.txt')
+       minioClient.fput_object('sq-test-bucket', 'file1.txt', 'data/file1.txt')
 except ResponseError as err:
        print(err)
