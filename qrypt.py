9cc0899c77e33563bbbb2053b28b431cfbc62f5d
maany <imptodefeat@gmail.com>
2020-11-28 19:52:22 +0100

    qrypt cipher/decipher

--- qrypt.py
+++ qrypt.py
@@ -2,18 +2,21 @@ from cryptography.fernet import Fernet
 
 
 class Qrypt:
-    def __init__(self):
-        pass
+    def __init__(self, key):
+        self.key = key
 
-    def generate_key(self):
+    @classmethod
+    def generate_key(cls):
         key = Fernet.generate_key()
         return key
 
-    def save_key(self, key, key_path):
+    @classmethod
+    def save_key(cls, key, key_path):
         with open(f"{key_path}", "wb") as key_file:
             key_file.write(key)
 
-    def load_key(self, key_path):
+    @classmethod
+    def load_key(cls, key_path):
         with open(f"{key_path}", "r") as key_file:
             return key_file.read()
 
@@ -23,9 +26,41 @@ class Qrypt:
     def recombine_key(self):
         pass
 
-    def encypt(self):
-        pass
+    def encrypt_file(self, file):
+        print('Reading File')
 
-    def decrypt(self):
-        pass
+        with open(file, "rb") as file:
+            file_data = file.read()
+            encrypted_data = self.key.encrypt(file_data)
 
+        print('Saving encrypted file')
+
+        with open(file, "wb") as file:
+            file.write(encrypted_data)
+
+    def decrypt_file(self, encrypted_file):
+        """
+            Given a filename (str) and key (bytes), it decrypts the file and write it
+        """
+        print(f"Reading Encrypted File {encrypted_file}")
+        with open(encrypted_file, "rb") as file:
+            # read the encrypted data
+            encrypted_data = file.read()
+        print(f"Decrypting File {encrypted_file}")
+
+        # decrypt data
+        decrypted_data = self.key.decrypt(encrypted_data)
+
+        print(f"Saving decrypted file {encrypted_file}")
+        # write the original files
+        with open(file, "wb") as file:
+            file.write(decrypted_data)
+
+    def encrypt_message(self, message):
+        print(f"Encrypting {message}")
+        return self.key.encypt(message)
+
+    def decrypt_message(self, encrypted_message):
+        decrypted = self.key.decrypt(encrypted_message)
+        print(f"Decrypted Message: {decrypted}")
+        return decrypted
--- sqlient.py
+++ sqlient.py
@@ -8,7 +8,6 @@ class SQlient:
         self.name = name
         self.buckets = [f"bucket-1-{name}"]
         self.mount10 = Mount10(self.name, server_ip, server_access_key, server_secret_key)
-        self.qrypt = Qrypt()
 
         home_dir = os.getenv("HOME")
         self.local_synergy_dir = f"{home_dir}/synergy-quantum"
@@ -20,10 +19,12 @@ class SQlient:
 
         self.key_path = f"{self.local_user_dir}/{self.name}.key"
         if not os.path.exists(self.key_path):
-            self.key = self.qrypt.generate_key()
-            self.qrypt.save_key(self.key, self.key_path)
+            self.key = Qrypt.generate_key()
+            Qrypt.save_key(self.key, self.key_path)
         else:
             self.key = self.qrypt.load_key(self.key_path)
+
+        self.qrypt = Qrypt(self.key)
 
         for bucket in self.buckets:
             try:
