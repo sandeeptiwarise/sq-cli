0874898686b01aa2cab5010853e924fa16a2bc6b
maany <imptodefeat@gmail.com>
2021-02-12 20:57:43 +0100

    fetch oauth2 token

--- sq_cli/sqauth.py
+++ sq_cli/sqauth.py
@@ -1,5 +1,7 @@
 import logging
 import base64
+import requests
+
 
 logger = logging.getLogger(__name__)
 
@@ -15,19 +17,26 @@ class SQAuth:
 
     @classmethod
     def generate_authorization_header(cls, app_id, app_secret):
-        authorization_string = base64.b64decode(f"{app_id}:{app_secret}")
-        authorization_header = f"Basic {authorization_string}"
+        authorization_string = f"{app_id}:{app_secret}"
+        authorization_string_bytes = authorization_string.encode('ascii')
+        base64_bytes = base64.b64encode(authorization_string_bytes)
+        base64_header = base64_bytes.decode('ascii')
+        authorization_header = f"Basic {base64_header}"
         logger.info(f"Generating authorization header: {authorization_header}")
         return authorization_header
 
     @classmethod
-    def generate_authorization_url(cls, url, client_id, redirect_uri):
+    def generate_authorization_endpoint_url(cls, url, client_id, redirect_uri):
         return f"https://{url}/oauth2/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}"
+
+    @classmethod
+    def generate_token_endpoint_url(cls, url):
+        return f"https://{url}/oauth2/token"
 
     @classmethod
     def generate_auth_key_url(cls):
         logger.info("Generating AWS Cognito URL for /authorize endpoint")
-        authorization_url = SQAuth.generate_authorization_url(SQAuth.URL, SQAuth.APP_ID, SQAuth.REDIRECT_URI)
+        authorization_url = SQAuth.generate_authorization_endpoint_url(SQAuth.URL, SQAuth.APP_ID, SQAuth.REDIRECT_URI)
         print(
             "Please open the following url in your browser. You will be asked to login with your SynergyQuantum account. "
             "After a successful login, you will be redirected to a local web page. Please copy the url of the local web page "
@@ -37,8 +46,21 @@ class SQAuth:
         print(f"{authorization_url}")
 
     @classmethod
-    def fetch_token(cls):
-        pass
+    def fetch_token(cls, auth_code):
+        url = SQAuth.generate_token_endpoint_url(SQAuth.URL)
+        authorization_header = SQAuth.generate_authorization_header(SQAuth.APP_ID, SQAuth.APP_SECRET)
+        token_request = requests.post(url,
+                                      data={
+                                          "grant_type": "authorization_code",
+                                          "client_id": SQAuth.APP_ID,
+                                          "code": auth_code,
+                                          "redirect_uri": SQAuth.REDIRECT_URI
+                                      },
+                                      headers={
+                                          "Content-Type": "application/x-www-form-urlencoded",
+                                          "Authorization": f"{authorization_header}"
+                                      })
+        print(token_request.text)
 
     @classmethod
     def verify_token(cls):
